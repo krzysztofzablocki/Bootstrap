41c79ff921fa9accf9a7f326f9a32b62e1e8b8bd
Dal Rupnik <legoless@gmail.com>
2014-10-30 13:29:39 +0100

    Script to work in any kind of Debug configuration
    If configuration's name was in another shape than "Debug" this script always failed to append branch. Updated the script to match the configuration with contains method, meaning branch will be appended even if configuration is called: "AppStore.Debug" or "AppStore-Debug".

--- Pod/Assets/Scripts/buildNumber.sh
+++ Pod/Assets/Scripts/buildNumber.sh
@@ -6,7 +6,7 @@ branch=`"$git" rev-parse --abbrev-ref HEAD`
 commit=`"$git" rev-parse --short HEAD`
 version=`/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${INFOPLIST_FILE}"`
 
-if [ $CONFIGURATION = "Debug" ]; then
+if [[ $CONFIGURATION == *Debug* ]]; then
   /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $build_num-$branch" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
 else
   /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $build_num" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
